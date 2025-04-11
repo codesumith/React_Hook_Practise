981eba7e73552f1316740bb2dee58fe6e2968485
codesumith <sumithkumar100@gmail.com>
2025-04-11 12:11:48 +0530

    added useMemo from dev branch

---
+++ src/USE_MEMO_EXAMPLES/usememotutorial.jsx
@@ -0,0 +1,45 @@
+import { useMemo, useState } from "react";
+import FetchAPI from "../CUSTOM_HOOKS/fetchDataFromApi";
+
+const MemoTutorial = () => {
+
+ const data =  FetchAPI("https://fakestoreapi.com/products");
+ const [show, setShow] = useState(false);
+
+
+const findLargeTitle = ()=>{
+    
+    if(!data) return "Loading...";
+
+    let largest = "";
+
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].title.length > largest.length) {
+          largest = data[i].title;
+        }
+      }
+
+    console.log("Function Excecuted")
+
+    return largest
+}
+
+const largestName = useMemo(()=>{
+     return findLargeTitle();
+},[data])
+
+return(
+    <div className="container">
+            <div className="box">
+                 <p>{largestName}</p>
+                 <button onClick={() => {
+                    setShow(!show)
+                 }}>Toggle</button>
+                 {show && "Toggle"}
+            </div>
+    </div>
+)
+}
+
+export default MemoTutorial;
